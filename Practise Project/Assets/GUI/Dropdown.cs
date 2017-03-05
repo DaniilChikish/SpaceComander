e4071f696167835a192f40e60372fc7dc22b6bfd
Daniil Chikish <daniil.chikish@mail.ru>
2017-03-06 00:57:29 +0200

    Upload GUI
    Upload GUI template.

---
+++ Practise Project/Assets/GUI/Dropdown.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Dropdown : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
+{
+    public RectTransform container;
+    public bool isOpen;
+    public Text maintext;
+    public Image image { get { return GetComponent<Image>(); } }
+
+    public List<DropDownChild> children;
+    public float childHeight = 30;
+    public int childFontSize = 11;
+    public Color normal = Color.white;
+    public Color highlighted = Color.red;
+    public Color pressed = Color.grey;
+   
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isOpen = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isOpen = false; 
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        container = transform.FindChild("Container").GetComponent<RectTransform>();
+        isOpen = false;
+	}
+	
+	// Update is called once per frame
+	public void Update ()
+    {
+        Vector3 scale = container.localScale;
+        scale.y = Mathf.Lerp(scale.y, isOpen ? 1:0, Time.deltaTime * 12);
+        container.localScale = scale;
+
+	}
+}
+
+
+[System.Serializable]
+public class DropDownChild
+{
+    public GameObject childObj;
+    public Text childText;
+    public Button.ButtonClickedEvent childEvents;
+
+    private LayoutElement element { get { return childObj.GetComponent<LayoutElement>(); } }
+    private Button button { get { return childObj.GetComponent<Button>(); } }
+    private Image image { get { return childObj.GetComponent<Image>(); } }
+
+    public DropDownChild(Dropdown parent)
+    {
+        childObj = UIUtility.NewButton("Child", "Button", parent.container).gameObject;
+        childObj.AddComponent<LayoutElement>();
+
+        childText = childObj.GetComponentInChildren<Text>();
+        childEvents = button.onClick;
+
+    }
+    public bool UpdateChild(Dropdown parent)
+    {
+        if (childObj == null)
+            return false;
+
+        element.minHeight = parent.childHeight;
+        
+
+        image.sprite = parent.image.sprite;
+        image.type = parent.image.type;
+
+        childText.font = parent.maintext.font;
+        childText.color = parent.maintext.color;
+        childText.fontSize = parent.maintext.fontSize;
+
+        ColorBlock b = button.colors;
+        b.normalColor = parent.normal;
+        b.highlightedColor = parent.highlighted;
+        b.pressedColor = parent.pressed;
+        
+        button.onClick = childEvents;
+
+
+        return true;
+    }
+}
---
+++ Practise Project/Assets/GUI/UIUtility.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIUtility : MonoBehaviour
+{
+    [MenuItem("GameObject/UI/Dropdown")]
+    public static void NewDropdown()
+    {
+        NewButton("Dropdown", "Dropdown", GetCanvas().transform).gameObject.AddComponent<Dropdown>();
+    }
+    public static Canvas GetCanvas()
+    {
+        Canvas c = FindObjectOfType<Canvas>();
+        if (c == null)
+        {
+            c = new GameObject().AddComponent<Canvas>();
+            c.name = "Canvas";
+            c.gameObject.layer = 5;
+            c.renderMode = RenderMode.ScreenSpaceOverlay;
+            c.gameObject.AddComponent<CanvasScaler>();
+            c.gameObject.AddComponent<GraphicRaycaster>();
+
+            if (FindObjectOfType<EventSystem>() == null)
+            {
+                EventSystem e = new GameObject().AddComponent<EventSystem>();
+                e.name = "Event System";
+                e.gameObject.AddComponent<StandaloneInputModule>();
+                e.gameObject.AddComponent<TouchInputModule>();
+            }
+
+        }
+        return c;
+    }
+    public static RectTransform NewUIElement(string name, Transform parent)
+    {
+        RectTransform temp = new GameObject().AddComponent<RectTransform>();
+        temp.name = name;
+        temp.gameObject.layer = 5;
+        temp.SetParent(parent);
+        temp.localScale = new Vector3(1, 1, 1);
+        temp.localPosition = new Vector3(0, 0, 0);
+
+        return temp;
+    }
+    public static Button NewButton(string name, string text, Transform parent)
+    {
+        RectTransform btnRect = NewUIElement(name, parent);
+        btnRect.gameObject.AddComponent<Image>();
+        btnRect.gameObject.AddComponent<Button>();
+        ScaleRect(btnRect, 100, 30);
+        NewText(text, btnRect); 
+       
+        return btnRect.GetComponent<Button>();
+    }
+    public static Text NewText(string text, Transform parent)
+    {
+        RectTransform textRect = NewUIElement("Text", parent);
+        Text t = textRect.gameObject.AddComponent<Text>();
+        t.text = text;
+        t.color = Color.black;
+        t.alignment = TextAnchor.MiddleCenter;
+        ScaleRect(textRect, 0, 0);
+        textRect.anchorMin = new Vector2(0, 0);
+        textRect.anchorMax = new Vector2(1,1);
+
+        return t;
+    }
+    public static void ScaleRect(RectTransform r, float w, float h)
+    {
+
+    }
+}
